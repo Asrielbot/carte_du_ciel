d2a1065c479ae6fe964eae042ced04b37a48c36c
A914570 <alexandre.pozzi@atos.net>
2025-01-14 18:21:22 +0100

    Added top 50 star size

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 
 import "./App.css";
 import { parseCSV } from "./utils";
+import Top_50_Array from "./components/Top_50_array.jsx";
 
 function App() {
+  const [stars, setStars] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,7 +16,18 @@ function App() {
         }
         const csvContent = await response.text();
         const data = await parseCSV(csvContent);
-        console.log(data);
+        // console.log(data.map((star) => star.ci));
+
+        // Filtrer les 50 étoiles les plus lumineuses
+        const filteredStars = data
+            .filter((star) => star.lum != null && star.lum > 0) // Supprimer les valeurs nulles
+            .sort((a, b) => b.lum - a.lum) // Trier par luminosité décroissante
+            .slice(0, 50); // Prendre les 50 premières
+
+        setStars(filteredStars);
+
+        // Générer un fichier JSON
+        // generateJSONFile(filteredStars);
       } catch (error) {
         console.error("Error parsing CSV: ", error);
       }
@@ -21,7 +35,22 @@ function App() {
     fetchData();
   }, []);
 
-  return <></>;
+  // Fonction pour générer et télécharger un fichier JSON
+  // const generateJSONFile = (data) => {
+  //   const jsonString = JSON.stringify(data, null, 2); // Convertir en JSON formaté
+  //   const blob = new Blob([jsonString], { type: "application/json" });
+  //   const link = document.createElement("a");
+  //   link.href = URL.createObjectURL(blob);
+  //   link.download = "top_50_stars.json"; // Nom du fichier
+  //   link.click();
+  // };
+
+  return (
+      <div className="App">
+        <h1>Top 50 Étoiles les Plus Lumineuses</h1>
+        <Top_50_Array data={stars}/>
+      </div>
+  );
 }
 
 export default App;
