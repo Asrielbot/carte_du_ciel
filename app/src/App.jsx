d8a7c203c04c846ce344b0c7eced8150a4e1f5f2
Asrielbot <49340417+Asrielbot@users.noreply.github.com>
2025-01-27 09:25:19 +0100

    add limit to shiniest stars

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -54,16 +54,16 @@ function App() {
     })
     .slice(0, 50);
 
-  // const shininessStars = stars
-  //   .map((star) => ({
-  //     ...star,
-  //     shininess: star.lum * (-15833.33 * star.ci + 33666.67),
-  //   }))
-  //   .filter((star) => star.shininess > 0);
+  const shininessStars = stars
+    .map((star) => ({
+      ...star,
+      shininess: star.lum * (-15833.33 * star.ci + 33666.67),
+    }))
+    .filter((star) => star.shininess > 0);
 
-  // const shiniestStars = shininessStars
-  //   .sort((a, b) => b.shininess - a.shininess)
-  //   .slice(0, 50);
+  const shiniestStars = shininessStars
+    .sort((a, b) => b.shininess - a.shininess)
+    .slice(0, 50);
 
   return (
     <div className="App">
@@ -74,7 +74,7 @@ function App() {
       <h1>Top 50 Étoiles les plus proches de la Terre</h1>
       <Top_50_Closest data={closestStars} />
       <h1>Top 50 Étoiles les plus brillantes</h1>
-      <Top_50_Shiniest data={stars} />
+      <Top_50_Shiniest data={shiniestStars} />
     </div>
   );
 }
